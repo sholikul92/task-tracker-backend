70a35fdd1381b082e591d684461983121bd45935
sholikul <sholikulardian@gmail.com>
2024-11-23 18:08:08 +0700

    seutp router

--- main.go
+++ main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"task-tracker/db"
+	"task-tracker/handler"
+	"task-tracker/repository"
+	"task-tracker/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := SetupRouter()
+
+	fmt.Println("Server runing in http://localhost:8080")
+	log.Fatal(router.Run(":8080"))
+}
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+	repo := repository.NewTaskRepository(db.Data{})
+	service := service.NewTaskService(repo)
+	handler := handler.NewTaskHandler(service)
+
+	task := r.Group("task")
+	{
+		task.POST("/add", handler.AddTask)
+		task.GET("/", handler.GetTasks)
+		task.GET("/:id", handler.GetTaskById)
+		task.PUT("/edit/:id", handler.EditTask)
+		task.DELETE("/delete/:id", handler.DeleteTask)
+	}
+
+	return r
+}
