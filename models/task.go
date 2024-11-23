242759246aea456c31fd73e5c14becbef6e0ef5c
sholikul <sholikulardian@gmail.com>
2024-11-23 18:10:58 +0700

    eleted model

--- models/task.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Task struct {
-	ID          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Completed   bool   `json:"completed"`
-}
