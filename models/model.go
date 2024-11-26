78280eac28a3e6915f64645fdc965277fcce4c73
sholikul <sholikulardian@gmail.com>
2024-11-26 18:45:46 +0700

    edit field task

--- models/model.go
+++ models/model.go
@@ -1,8 +1,11 @@
 package models
 
+import "time"
+
 type Task struct {
-	ID          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Completed   bool   `json:"completed"`
+	ID        int       `json:"id"`
+	Title     string    `json:"title" binding:"required"`
+	Priority  string    `json:"priority" binding:"required"`
+	Deadline  time.Time `json:"deadline" binding:"required"`
+	Completed bool      `json:"completed"`
 }
