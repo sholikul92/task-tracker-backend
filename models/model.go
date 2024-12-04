bdf409f17709d9817de0160634e3fb03ad163c7b
sholikul <sholikulardian@gmail.com>
2024-12-04 15:26:11 +0700

    fix: added gorm.model in task struct adn define dbconfig struct

--- models/model.go
+++ models/model.go
@@ -1,11 +1,23 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Task struct {
-	ID        int       `json:"id"`
+	gorm.Model
 	Title     string    `json:"title" binding:"required"`
 	Priority  string    `json:"priority" binding:"required"`
 	Deadline  time.Time `json:"deadline" binding:"required"`
 	Completed bool      `json:"completed"`
+}
+
+type DBConfig struct {
+	HOST     string
+	PORT     int
+	USER     string
+	PASSWORD string
+	DBNAME   string
 }
